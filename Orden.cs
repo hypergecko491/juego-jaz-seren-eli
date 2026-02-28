b1bcd08bf7a3832db7251bc6b0b40066051bbbca
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-02-28 13:42:50 -0600

    Refactor order initialization in Orden class

--- Orden.cs
+++ Orden.cs
@@ -35,75 +35,53 @@ public class Orden : MonoBehaviour
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+{
+    registradora = GameObject.Find("Registradora").GetComponent<Registradora>();
+    camTransform = GameObject.Find("Camara1").transform;
+
+    int randomTopping = Random.Range(0, topping.Length);
+    toppingPlace.sprite = topping[randomTopping];
+
+    int randomSalsa = Random.Range(0, salsa.Length);
+    salsaPlace.sprite = salsa[randomSalsa];
+
+    int randomCamaron = Random.Range(0, camaron.Length);
+    camaronPlace.sprite = camaron[randomCamaron];
+
+    int randomVaso = Random.Range(0, vasos.Length);
+    vasosPlace.sprite = vasos[randomVaso];
+
+    switch (randomTopping)
     {
-       registradora = GameObject.Find("Registradora").GetComponent<Registradora>();
-       
-       camTransform = GameObject.Find("Camara1").transform;
-
-        int randomTopping = Random.Range(0, topping.Length);
-        toppingPlace.sprite = topping[randomTopping];
-        int randomSalsa = Random.Range(0, salsa.Length);
-        salsaPlace.sprite = salsa[randomSalsa];
-        int randomCamaron = Random.Range(0, camaron.Length);
-        camaronPlace.sprite = camaron[randomCamaron];
-        int randomVaso = Random.Range(0, vasos.Length);
-        vasosPlace.sprite = vasos[randomVaso];
-
-        switch (topping.Length)
-        {
-            case 0:
-                toppingOrder = "pepino";
-                break;
-            case 1:
-                toppingOrder = "aguacate";
-                break;
-            case 2:
-                toppingOrder = "chile";
-                break;
-        }
-        switch (vasos.Length)
-        {
-            case 0:
-                vasoOrder = "chico";
-                break;
-            case 1:
-                vasoOrder = "grande";
-                break;
-        }
-        switch(salsa.Length)
-        {
-            case 0:
-                salsaOrder = "clamato";
-                break;
-            case 1:
-                salsaOrder = "chevecha";
-                break;
-            case 2:
-                salsaOrder = "aguaChile";
-                break;
-            case 3:
-                salsaOrder = "breMis";
-                break;
-        }
-        switch(camaron.Length)
-        {
-            case 0:
-                camaronOrder = "crudo";
-                break;
-            case 1:
-                camaronOrder = "cocido";
-                break;
-            case 2:
-                camaronOrder = "empanizado";
-                break;
-        }
-        thisNumOrder = registradora.GetnumOrder();
-      
-        print(thisNumOrder);
-
-        randomSpawnPosition = new Vector3(Random.Range(x1, x2), Random.Range(y1, y2), 0);
-
+        case 0: toppingOrder = "pepino"; break;
+        case 1: toppingOrder = "aguacate"; break;
+        case 2: toppingOrder = "chile"; break;
     }
+
+    switch (randomVaso)
+    {
+        case 0: vasoOrder = "chico"; break;
+        case 1: vasoOrder = "grande"; break;
+    }
+
+    switch (randomSalsa)
+    {
+        case 0: salsaOrder = "clamato"; break;
+        case 1: salsaOrder = "chevecha"; break;
+        case 2: salsaOrder = "aguaChile"; break;
+        case 3: salsaOrder = "breMis"; break;
+    }
+
+    switch (randomCamaron)
+    {
+        case 0: camaronOrder = "crudo"; break;
+        case 1: camaronOrder = "cocido"; break;
+        case 2: camaronOrder = "empanizado"; break;
+    }
+
+    thisNumOrder = registradora.GetnumOrder();
+    randomSpawnPosition = new Vector3(Random.Range(x1, x2), Random.Range(y1, y2), 0);
+}
 
     public string GetOrderTop()
     {
@@ -128,3 +106,4 @@ public class Orden : MonoBehaviour
         transform.position = camTransform.position + offsets + randomSpawnPosition;
     }
 }
+
